cc89fcb77709651e93d0eefb1fa759299b1e5585
dominik-nagy27 <giganiga7711@gmail.com>
2024-10-09 14:42:31 +0200

    Teszt

--- Teszt/Program.cs
+++ Teszt/Program.cs
@@ -9,7 +9,17 @@ namespace Teszt
     class Program
     {
         static void Main(string[] args)
-        {//Az szép
+        {
+            int x = 100,
+                y = 11;
+
+            Console.WriteLine($"Az 1. iratás x={x} kiírása előtt:{x++}");
+            Console.WriteLine($"A 2.iratás y={x} kiírása előtt:++y = {x++}");
+            Console.WriteLine($"Az 1. iratás y={y} kiírása előtt:{y++}");
+            Console.WriteLine($"A 2.iratás y={y} kiírása előtt:++y = {y++}");
+            Console.ReadKey();
+
+
         }
     }
 }
